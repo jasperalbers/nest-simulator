d994b8caa0adea34da2a43f7989b77f5303899fa
jarsi <jari.pronold@gmail.com>
2019-06-10 08:56:38 +0200

    Include Timers, activate with -DTIMER=1 or -DTIMER=ON
    Conflicts:
    	nestkernel/simulation_manager.cpp

--- nestkernel/event_delivery_manager.h
+++ nestkernel/event_delivery_manager.h
@@ -235,6 +235,9 @@ public:
    */
   virtual void reset_timers_counters();
 
+  Stopwatch sw_communicate_spike_data;
+  Stopwatch sw_deliver_spike_data;
+
 private:
   template < typename SpikeDataT >
   void gather_spike_data_( const thread tid,
